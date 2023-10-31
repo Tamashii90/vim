ee3337b7ce192c8cceb5078ee9dbd72dace6fea8
Tamashii90 <patoboy90@hotmail.com>
2023-10-31 07:23:47 +0200

    tweak some lsp settings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -10,20 +10,14 @@ local on_attach = function(client, bufnr)
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  -- vim.keymap.set("n", "gl", vim.diagnostic.open_float, bufopts) // This is handled by telescope!
+  vim.keymap.set('n', 'ge', vim.diagnostic.open_float, bufopts)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  -- vim.keymap.set("n", "gl", vim.diagnostic.open_float, bufopts)
   -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts) // This is handled by telescope!
   vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
   vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format({ bufnr = bufnr, timeout_ms = 20000 }) end, bufopts)
   vim.api.nvim_create_autocmd("BufWritePre", {
     buffer = bufnr,
@@ -44,13 +38,27 @@ null_ls.setup({
   on_attach = on_attach,
 })
 
-local lsp = require('lsp-zero')
+local lsp = require('lsp-zero').preset('recommended')
+lsp.setup()
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require('luasnip.loaders.from_vscode').lazy_load({ paths = { "./snippets.lua" } })
 
-lsp.preset('recommended')
 
-lsp.setup()
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = true,
+  float = {
+    focusable = true,
+    style = 'minimal',
+    border = 'rounded',
+    source = 'always',
+    header = '',
+    prefix = '',
+  },
+})
 
 require('lspconfig').lua_ls.setup {
   settings = {
@@ -181,15 +189,6 @@ cmp.setup.cmdline(':', {
   }, {
     { name = 'cmdline' }
   })
-})
-
-vim.diagnostic.config({
-  virtual_text = true,
-  signs = false,
-  update_in_insert = false,
-  underline = true,
-  severity_sort = false,
-  float = true,
 })
 
 local servers = { 'clangd', 'rust_analyzer' }
